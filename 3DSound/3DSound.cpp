50001325239c6d50988d9b6049e8b7de301da5ad
Felix Willebrand Westin <fewes17@gmail.com>
2017-09-20 14:48:06 +0200

    Reverted to NSound and made a fucksweet program with it

--- 3DSound/3DSound.cpp
+++ 3DSound/3DSound.cpp
@@ -3,7 +3,9 @@
 
 #include "stdafx.h"
 #include <SFML/Window.hpp>
+#include <Nsound/NsoundAll.h>
 
+using namespace Nsound;
 
 int main()
 {
@@ -18,6 +20,33 @@ int main()
 				window.close();
 		}
 	}
+
+	float64 sr = 44100.0;
+
+    // Create a new instance of the Sine Generator
+    Sine sine(sr);
+
+    Buffer pan;
+
+    pan << sine.generate(1.0, 3.0);
+
+    // Create a stereo AudioStream.
+    AudioStream as(sr, 2);
+
+    // Fill it with a 220 Hz sine wave.
+    as << 0.5 * sine.generate(4.9, 220);
+
+    // Execute the pan method.
+    as.pan(pan);
+
+    // Write the AudioStream to a wave file
+    as >> "example2.wav";
+
+    // Play it through the sound card
+
+    AudioPlayback pb(sr, 2, 16);
+
+    as >> pb;
 
     return 0;
 }
