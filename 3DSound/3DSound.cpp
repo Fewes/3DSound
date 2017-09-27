e7a4e61985c6fb1beae011346050ec10283ad0ca
Felix Willebrand Westin <fewes17@gmail.com>
2017-09-27 14:56:46 +0200

    Added ImGui and cleaned up main file

--- 3DSound/3DSound.cpp
+++ 3DSound/3DSound.cpp
@@ -3,80 +3,73 @@
 
 #include "stdafx.h"
 #include "SoundNode.h"
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include <Nsound/NsoundAll.h>
 #include <iostream>
+#include <imgui.h>
+#include <imgui-SFML.h>
 
 using namespace Nsound;
+using namespace sf;
 
 int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "My window");
+	// Create a window
+	RenderWindow window(VideoMode(1280, 720), "3DSound");
+	window.setFramerateLimit(60);
+
+	// Initialize ImGui
+    ImGui::SFML::Init(window);
+
+	// Initialize frame clock
+	Clock clock;
 
 	while (window.isOpen())
 	{
-		sf::Event event;
+		Time dt = clock.restart();
+
+		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			// Poll ImGui events
+			ImGui::SFML::ProcessEvent(event);
+
+			if (event.type == Event::Closed)
 				window.close();
 		}
+
+		// Update ImGui
+		ImGui::SFML::Update(window, dt);
+
+        ImGui::Begin("Amazing 3D Sound Generator");
+        if (ImGui::Button("Generate 3D Sound"))
+		{
+			SoundNode sound("x1.wav", vector<double>(0, 3));
+			sound.generate3D();
+			// Make a two channel AudioStream with right samplerate
+			AudioStream as(sound.getSampleRate(), 2);
+
+			// Assign left sound to the left (AudioStream[0]) buffer in AudioStream 
+			as[0] << sound.getChannel(0);
+			// Assign right sound to the left (AudioStream[1]) buffer in AudioStream
+			as[1] << sound.getChannel(1);
+
+			// Make a playback "source" that plays two channels and has right samplerate and 16bit/sample
+			AudioPlayback pb(sound.getSampleRate(), 2, 16);
+
+			// Play the AudioStream in the AudioPlayback
+			as >> pb;
+		}
+        ImGui::End();
+
+		// Draw stuff
+		window.clear(Color(46, 46, 46, 255));
+        ImGui::SFML::Render(window);
+        window.display();
 	}
 
-	/*
-	//To olny test right and left ear sound
-	AudioStream LAudio("x1.wav"); //Load left ear audio. Need to be AudioStream to control numofchannels. 
-	LAudio.mono(); //Force it to Mono to be sure it ony have one channel
-	AudioStream RAudio("Temperature_in.wav");;//Load right ear audio. Need to be AudioStream to control numofchannels. 
-	RAudio.mono(); //Force it to Mono to be sure it ony have one channel
-
-	RAudio.normalize(); //Normalize the sound to get a better level
-	LAudio.normalize();
-	*/
-
-	
-	//Check the HRFT. (For me it works :O 
-	/*AudioStream RAudio("x1.wav"); //Load right ear audio. Need to be AudioStream to control numofchannels
-	RAudio.mono(); //Force it to Mono to be sure it ony have one channel
-	AudioStream LAudio("x1.wav"); //Only nesscary to make one left and one right if we want to handel them seperate before convolution
-	LAudio.mono(); 
-	int angle = 90; //Hardcoded angle (to the right)
-	int elevation = -40; //Hardcoded evelation (level zero)
-	Buffer Lb("HRTFdata/L" + std::to_string(elevation) + "e0" + std::to_string(angle) + "a.wav"); //Load hrftdata for left channel at the right ang and ev
-	Buffer Rb("HRTFdata/R" + std::to_string(elevation) + "e0" + std::to_string(angle) + "a.wav"); //Load hrftdata for right channel at the right ang and ev
-	LAudio.convolve(Lb); //Make a convolution on leftaudio with leftchannel hrft;
-	RAudio.convolve(Rb); //Make a convolution on rightaudio with rightchannel hrft; 
-	
-	/*
-	float64 sr = 44100.0; //Set the samplerate (this should accually be dynamic TODO)
-
-	AudioStream as(sr, 2); //Make a two channel audiostream with right samplerate
-
-	as[0] << LAudio; //Asign left sound to the left (audiostreamname[0]) buffer in audiostream 
-	as[1] << RAudio; //Asign right sound to the left (audiostreamname[1]) buffer in audiostream
-	*/
-
-	//SoundNode sound("L:/Erik/Music/Musik/Fronda/Svart Poesi/15 Nu för tiden.wav", vector<double>(0, 3));
-	SoundNode sound("x1.wav", vector<double>(0, 3));
-	sound.generate3D();
-	AudioStream as(sound.getSampleRate(), 2); //Make a two channel audiostream with right samplerate
-
-	as[0] << sound.getChannel(0); //Asign left sound to the left (audiostreamname[0]) buffer in audiostream 
-	as[1] << sound.getChannel(1); //Asign right sound to the left (audiostreamname[1]) buffer in audiostream
-
-	AudioPlayback pb(sound.getSampleRate(), 2, 16); //Make a playback "source" that plays two channels and has right samplerate and 16bit/sample
-
-	as >> pb; //Play the audiostream in the audioplayback
+	// Play nice and kill ImGui before exiting
+	ImGui::SFML::Shutdown();
 	
     return 0;
 }
-
-//ab.plot("testing");
-//FFTransform t(44100.0);
-/*FFTChunkVector abf = t.fft(ab, 2);
-FFTChunkVector Lbf = t.fft(Lb, 2);
-
-//FFTChunkVector resultf = abf*Lbf;
-std::cout << abf.getOriginalSize();
-Buffer result = t.ifft(resultf);
-*/
